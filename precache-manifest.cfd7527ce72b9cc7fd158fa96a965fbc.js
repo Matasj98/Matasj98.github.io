1ae09f804f8184301d068deae837a9518dbc806f
Matas <matas.jarilinas@stud.vgtu.lt>
2019-09-14 12:25:50 +0300

    final build

---
+++ precache-manifest.cfd7527ce72b9cc7fd158fa96a965fbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "defd198b6a7caa33af263e482a95fe60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80957367fc1865152e9b",
+    "url": "/static/css/main.f7db2036.chunk.css"
+  },
+  {
+    "revision": "d99549ea795812f33249",
+    "url": "/static/js/2.c3a2534f.chunk.js"
+  },
+  {
+    "revision": "80957367fc1865152e9b",
+    "url": "/static/js/main.69aca577.chunk.js"
+  },
+  {
+    "revision": "38487fa761662558d89d",
+    "url": "/static/js/runtime~main.498583be.js"
+  },
+  {
+    "revision": "a8d4af5c6143c2b2aa0481d316ab3f0f",
+    "url": "/static/media/MyPicCroped.a8d4af5c.jpg"
+  },
+  {
+    "revision": "2fc71266593951afd32f6e0c44e736f9",
+    "url": "/static/media/logo3.2fc71266.png"
+  },
+  {
+    "revision": "c3e7f3b35a2bab09b31e1cf9c064d7d1",
+    "url": "/static/media/pr1.c3e7f3b3.png"
+  },
+  {
+    "revision": "3b1ddc3bfbd0e269bba571a885b3cabd",
+    "url": "/static/media/pr2.3b1ddc3b.png"
+  }
+]);
